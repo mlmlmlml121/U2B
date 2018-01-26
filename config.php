4e068ee9ce7212aa9ef4134467f7f9047aa55208
mlmlmlml121 <mlmlmlml121@cntv.cn>
2018-01-26 20:45:17 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyBuMFc5qGOO3eRW0nOnCLU6nkeNl5vLcZ0');
+define('GJ_CODE', 'jp');
+define('SITE_NAME', 'U2B');
+define('TITLENAME', 'U2B');
+define('EN2DEKEY', '.FDC|"9$9X~A9r^|>1l-gFU]nQ3,FvLYDM[T~hRo5y;jdo#ZId|M^[M|J.~XXx$');
+define('EMAIL', 'l3l451921@gmail.com');
+?>
